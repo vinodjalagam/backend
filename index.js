8f2012f9fc48d57509aa4edb94adc727402b0d0b
vinodjalagam <vinodjalagam477@gmail.com>
2024-07-24 16:34:50 +0530

    innitial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 // Route: /sayHello
 app.get('/sayHello', (req, res) => {
-    res.json({ message: 'Hello User' });
+    res.json({ message: 'Hello vinod kumar' });
 });
 
 // Start the server
