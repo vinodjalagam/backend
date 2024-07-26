3be4244b5f6593256a673f543a9eb17007b9fd09
vinodjalagam <63002962+vinodjalagam@users.noreply.github.com>
2024-07-26 22:40:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/sayHello', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
