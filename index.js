2085ef9facfbd4478fe177cbb71d975944d9e139
vinodjalagam <63002962+vinodjalagam@users.noreply.github.com>
2024-07-28 10:39:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 
 // Route: /sayHello
 app.get('/sayHello', (req, res) => {
-    res.json({ message: 'Hello vinod kumar goud' });
+    res.json({ message: 'Hello vinod kumar goud vinoddjbiobsw' });
 });
 
 // Start the server
-const PORT = process.env.PORT || 8086;
+const PORT = process.env.PORT || 8085;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
