9e4e3ac517db3c9ad4e63c80a19cbb32029598f6
vinodjalagam <63002962+vinodjalagam@users.noreply.github.com>
2024-07-29 21:47:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 // Route: /sayHello
 app.get('/sayHello', (req, res) => {
-    res.json({ message: 'Hello Vinod Kumar Goud ' });
+    res.json({ message: 'Hello vinod Vinod Kumar Goud ' });
 });
 
 // Start the server
